4bc6138bdb6af9a2302b2942ddd440dfc8243771
Syazwan Syafiq <s.syafiq194@gmail.com>
2020-04-21 15:20:25 +0800

    Update app.config.ts

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,10 +1,5 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
-    name: process.env.APP_NAME || 'Watchlist Service',
-    url: process.env.APP_URL || 'http://localhost:3000',
     timeZone: process.env.TZ,
-    withCron: process.env.APP_WITH_CRON,
-    country: process.env.APP_COUNTRY,
-    lang: process.env.APP_LANG,
 }));
